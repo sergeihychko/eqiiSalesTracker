5ea09e4a9e6a1becaedcbb0dfa150f93295211d7
Serge Hychko <shychko@gmail.com>
2024-09-23 22:37:47 -0400

    Implemented the treeview as defined by the dataframe object. Implemented the ability to sort on any of the columns in the dataframe.

--- mainseller.py
+++ mainseller.py
@@ -3,6 +3,8 @@ from operator import itemgetter
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+import pandas as pd
+#custom imports
 import dirutils
 import driver
 import filereader
@@ -56,9 +58,8 @@ def dc(event):
 
 def create_window():
     global detail_pane
-    global details_frame
     detail_pane = tk.Toplevel()
-    detail_pane.geometry("600x340")
+    detail_pane.geometry("500x340")
     detail_pane.title("Sales Details")
     varD=StringVar()
     mlist = utilObj.directoryList(output_directory)
@@ -68,24 +69,18 @@ def create_window():
         file_list = mlist
     varD.set(file_list[0])
     det_label = ttk.Label(detail_pane, text='Detail Pane')
-    det_label.pack()
-    drop_details = OptionMenu(detail_pane, varD, *file_list, command=repopulate_tree)
-    drop_details.pack()
-
-    details_frame = ttk.Treeview(detail_pane)
-    details_frame['columns'] = ("Item", "Price")
-    details_frame.column("#0", width=100, minwidth=90)
-    details_frame.column("Item",anchor=W, width=240)
-    details_frame.column("Price", anchor=CENTER, width=100)
-    details_frame.heading("#0", text="Label", anchor=W)
-    details_frame.heading("Item", text="Item", anchor=W)
-    details_frame.heading("Price", text="Price", anchor=CENTER)
-    populate_detail(details_frame,'')
-    details_frame.pack(pady=1)
+    det_label.grid(row=0, column=0)
+    drop_details = OptionMenu(detail_pane, varD, *file_list, command=repopulate_detail)
+    drop_details.grid(row=1, column=0)
+    populate_detail(detail_pane, '')
     detail_pane.mainloop()
 
+def repopulate_detail(varD):
+    print(" repopulate_detail :" + varD)
+    populate_detail(detail_pane, varD)
+
 def populate_detail(panel, varD):
-    print("populating detail_pane")
+    global df
     filereaderObj = filereader.Filereader()
     filereaderObj.rootDir = output_directory
     if len(varD) == 0:
@@ -95,12 +90,18 @@ def populate_detail(panel, varD):
     file_list = filereaderObj.getRows()
     if len(file_list) == 0:
         print("The file " + filereaderObj.fileToSearch + " was empty!")
+        repopulate_tree(varD)
     else:
+        ids = []
+        dates = []
+        items = []
+        prices = []
+        index = 0
         for i in file_list:
-            print("detail is : " + i)
-            price = ""
-            datestamp = ""
-            item = ""
+            index = index + 1
+            price = None
+            datestamp = None
+            item = None
             if i.find("for "):
                 itemdelimited = i.split("for ")
                 item = itemdelimited[0]
@@ -108,13 +109,49 @@ def populate_detail(panel, varD):
                     pricetime = itemdelimited[1].split("\\")
                     price = pricetime[0]
                     datestamp = pricetime[1]
-            panel.insert(parent='', index='end', text=datestamp, values=(item, price))
+                    ids.append(index)
+                prices.append(price)
+                items.append(item)
+                dates.append(datestamp)
+        data = {
+            "id": ids,
+            "Date": dates,
+            "Item": items,
+            "Price": prices
+        }
+        df = pd.DataFrame(data)
+        display_tree(panel, df)
+
+def display_tree(pane, df):
+    global details_window
+    dataframe_list = list(df)
+    result_set = df.to_numpy().tolist()
+    details_window = ttk.Treeview(pane, selectmode='browse',
+                                  show='headings', height=10, columns=dataframe_list)
+    details_window.grid(row=2, column=0, columnspan=3, padx=15, pady=10)
+    for col in dataframe_list:
+        details_window.column(col, width=100, anchor='c')
+        details_window.heading(col, text=col, command=lambda col=col: sort_on_column(col))
+    for dt in result_set:
+        v = [r for r in dt]  # creating a list from each row
+        details_window.insert("", 'end', iid=v[0], values=v)
+
+def sort_on_column(column):
+    global df,sort_order
+    if sort_order:
+        sort_order=False # set ascending value
+    else:
+        order=True
+    df=df.sort_values(by=[column],ascending=sort_order)
+    display_tree(detail_pane, df)
 
 def repopulate_tree(varD):
-    for row in details_frame.get_children():
-        details_frame.delete(row)
-    detail_pane.update()
-    populate_detail(details_frame, varD)
+    try:
+        for row in details_window.get_children():
+            details_window.delete(row)
+        detail_pane.update()
+    except NameError:
+        print("first time, panel not defined")
 #end method definitions
 
 root = Tk()
@@ -128,6 +165,7 @@ application_title = config.get('main-section', 'application_title')
 output_directory = config.get('main-section', 'output_directory')
 print(application_title + ":" + output_directory)
 root.title("Everquest II Seller monitor")
+sort_order = True
 
 utilObj = dirutils.Dirutils()
 currentPathToGenerate = ""
